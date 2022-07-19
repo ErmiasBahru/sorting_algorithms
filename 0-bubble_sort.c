bf386465c36cfbf7e12652da099e7fe12a1f85a4
Ermias Bahru <ermiasbahru878@gmail.com>
2022-07-19 05:25:09 -0400

    0. Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers with Bubble Sort
+ * @array: param
+ * @size: param
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+size_t i, n, n1;
+int swap;
+
+if (array == NULL || size < 2)
+return;
+
+n = size;
+while (n > 0)
+{
+n1 = 0;
+for (i = 0; i < n - 1; i++)
+{
+if (array[i] > array[i + 1])
+{
+swap = array[i];
+array[i] = array[i + 1];
+array[i + 1] = swap;
+n1 = i + 1;
+print_array(array, size);
+}
+}
+n = n1;
+}
+}
